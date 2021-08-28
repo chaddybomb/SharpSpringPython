f610452a55eb4324cb62f706b95fdc0938e8be8c
chaddybomb <chad@cmg.xyz>
2021-08-27 20:37:29 -0400

    Update ssp.py
    Changed the dataframe procedure so it's less expensive.

--- ssp.py
+++ ssp.py
@@ -17,13 +17,18 @@ email_addresses = ""
 ############# End Configuration ###############
 ###############################################
 
-import requests, json, string, random, pandas as pd
+import requests, json, string, random, os, pandas as pd
 
 def rand_id_gen(size=36, chars=string.ascii_uppercase + string.digits):
 	#uses string and random modules to create a random 36 char generated ID for the request
 	return ''.join(random.choice(chars) for _ in range(size))
 
+if os.path.exists("data.csv"): #if the data.csv file exists, remove so we don't append to an older version
+    os.remove("data.csv")
+
 email_list = email_addresses.split(",") #create list object
+
+df = pd.DataFrame(); #create a DataFrame
 
 for email in email_list: #run through each e-mail in the list
 
@@ -31,6 +36,7 @@ for email in email_list: #run through each e-mail in the list
 	#https://help.sharpspring.com/hc/en-us/sections/115000320047-API
 
 	url = f"https://api.sharpspring.com/pubapi/v1/?accountID={accountID}&secretKey={secretKey}"
+
 	payload = '''{
 	"method": "getLeads",
 	"params": {
@@ -44,11 +50,8 @@ for email in email_list: #run through each e-mail in the list
 	headers = {'Content-Type': 'text/plain'} #nothing special
 	response = requests.request("POST", url, headers=headers, data=payload) #post the payload
 	info = json.loads(response.text) #load the response with the json module
-	df = pd.json_normalize(info['result']['lead']) #convert json data into a flat table with pandas
 
-	#if this is the first email in the loop, create the file and add the header
-	#otherwise, just add a new line
-	if email == email_list[0]:
-		df.to_csv("data.csv", index=False)
-	else:
-		df.to_csv("data.csv", mode='a', header=False, index=False)
+	row = pd.json_normalize(info['result']['lead']) #get the json ready for the DataFrame
+	df = df.append(row, ignore_index=True) #append to the DataFrame
+
+df.to_csv("data.csv", mode='a', index=False) #push DataFrame into a CSV
